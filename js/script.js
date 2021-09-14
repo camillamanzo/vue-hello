499a372b0b7711a65ebfeefd8bc65dc6f7d52734
camillamanzo <camillasouthafrica@gmail.com>
2021-09-14 18:55:54 +0200

    finalized trial vue framework

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+let root = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "Hello Everybody"
+        }
+    }
+);
